9e7d2d8d7d9580fc49e4fedf9a72fc1b4b887141
eazyyz <eazyyyyyyz@gmail.com>
2025-04-23 20:10:54 +0500

    “update_and_delete”

--- accounts/views.py
+++ accounts/views.py
@@ -3,20 +3,23 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponseRedirect
 
+# from mysite.accounts.forms import SignupForm, LoginForm
+from .forms import SignupForm, LoginForm
+
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return HttpResponseRedirect('/articles')
+            return redirect('homepage')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
@@ -24,7 +27,7 @@ def login_view(request):
                 return redirect(request.POST['next'])
             return redirect('homepage')
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
 
 def logout_view(request):
--- articles/forms.py
+++ articles/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from . import models
 
-class CreateArticle(forms.ModelForm):
+class ArticleForm(forms.ModelForm):
     class Meta:
         model = models.Article
         fields = ['title', 'body', 'thumbnail']
+        labels = {
+            'title': 'Title',
+            'body': 'Text',
+            'thumbnail': 'Thumbnail',
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Write article title', 'class': 'form-control mb-3'}),
+            'body': forms.Textarea(attrs={'placeholder': 'Write article title', 'class': 'form-control mb-3'}),
+            'thumbnail': forms.FileInput(attrs={'class': 'form-control mb-3'}),
+        }
